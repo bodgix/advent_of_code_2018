d3e5750822b326fa0210cae3984883d904ca2ecc
Bogdan Katynski <bogdan.katynski@workday.com>
2018-12-02 00:43:20 +0000

    Add documentation

--- lib/day1_star1.ex
+++ lib/day1_star1.ex
@@ -1,6 +1,22 @@
-defmodule Day1Star2 do
+defmodule Day1Star1 do
+  @moduledoc """
+  Solution to the first part of Day 1 puzzle.
+  """
   @file_name "input"
 
+  @doc """
+  Sum the "frequencies"
+
+  The input data is called frequencies in the task description.
+  They're just positive or nefative integers.
+
+  The function takes a stream, converts strings to integers
+  using `Stream.map`. Stream is used so that not to evaluate the stream
+  but continue evaluating it lazily.
+
+  Returns:
+    The sum of all numbers from the input file.
+  """
   def sum_frequencies(io_stream) do
     io_stream
     |> Stream.map(fn freq_str ->
@@ -12,6 +28,12 @@ defmodule Day1Star2 do
     end)
   end
 
+  @doc """
+  Opens the input file as a lazyly evaluated stream
+  and passes it to `sum_frequencies`
+
+  Prints the result.
+  """
   def main do
     File.stream!(@file_name, [:read])
     |> sum_frequencies()
--- lib/day1_star2.ex
+++ lib/day1_star2.ex
@@ -1,6 +1,12 @@
-defmodule Day1Star1 do
+defmodule Day1Star2 do
   @file_name "input"
 
+  @doc """
+  Sums all numbers from the input stream until a partial sum repeats
+  itself.
+
+  Using `Enum.reduce_while` to break out when a repeating partial sum is found.
+  """
   def find_repeat(stream) do
     stream
     |> Stream.map(fn freq_str ->
@@ -20,6 +26,12 @@ defmodule Day1Star1 do
     |> Map.get(:sum)
   end
 
+  @doc """
+  Opens the input file as a stream, passes it to `Stream.cycle`
+  to create a looped infinite stream.
+
+  Passes the looped stream to find_repeat and prints the reult.
+  """
   def main do
     File.stream!(@file_name, [:read])
     |> Stream.cycle()
