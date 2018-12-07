89998a4b02cbb8917efb6872a06c84d851885888
Bogdan Katynski <bogdan.katynski@workday.com>
2018-12-07 23:56:05 +0000

    Remove debugging code

--- lib/day6_star2.ex
+++ lib/day6_star2.ex
@@ -7,44 +7,36 @@ defmodule Day6Star2 do
   def points_in_safe_distance(points_to_check, all_points, safe_distance) do
     sum =
       points_to_check
-      # |> IO.inspect(label: "Square")
       |> Enum.map(fn point ->
         dists_sum =
           all_points
           |> List.delete(point)
           |> manhatan_distances(point)
-          # |> IO.inspect(label: "Manhattan distances to other points")
           |> Enum.sum()
-          # |> IO.inspect(label: "Sum")
 
         if dists_sum < safe_distance, do: 1, else: 0
       end)
-      # |> IO.inspect(label: "Square safe points")
       |> Enum.sum()
-      # |> IO.inspect(label: "sum")
-
-    # IO.gets("Press any key")
 
     sum
   end
 
   def safe_area_around_point(points, {_x, _y} = point, safe_distance) do
-    IO.puts("Finding safe area around #{inspect(point)}")
-
     {area, _square} =
       Stream.iterate(0, &(&1 + 1))
       |> Enum.reduce_while({0, MapSet.new()}, fn radius, {area, last_square} ->
-        new_square =
-          build_square(point, radius)
+        new_square = build_square(point, radius)
 
         partial_area =
           new_square
           |> MapSet.difference(last_square)
           |> points_in_safe_distance(points, safe_distance)
 
-        if partial_area > 0, do: {:cont, {area + partial_area, new_square}}, else: {:halt, {area, new_square}}
+        if partial_area > 0,
+          do: {:cont, {area + partial_area, new_square}},
+          else: {:halt, {area, new_square}}
       end)
-    IO.puts("Total Area: #{area}")
+
     area
   end
 
@@ -59,19 +51,10 @@ defmodule Day6Star2 do
     points =
       File.stream!(@input_file, [:read])
       |> parse_input()
-      # |> Enum.sort_by(fn pt -> manhattan_distance({0, 0}, pt) end)
 
-    # list_size = Enum.count(points)
-    # middle_point =
-    #   points
-    #   |> Enum.at(div(list_size, 2))
-    # frame =
-    #   points
-    #   |> generate_frame()
     points
     |> Enum.map(fn point ->
       safe_area_around_point(points, point, @safe_distance)
     end)
-    |> IO.inspect(label: "Area sizes")
   end
 end
